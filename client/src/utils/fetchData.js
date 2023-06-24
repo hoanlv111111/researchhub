0fb9e6644d5ebff70324eee6294b2b348d3e4155
hoanlv111111 <135534728+hoanlv111111@users.noreply.github.com>
2023-06-24 10:12:41 +0700

    Update fetchData.js

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -8,28 +8,28 @@ export const getDataAPI = async (url, token) => {
 }
 
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`/api/${url}`, post, {
+    const res = await axios.post(`https://researchhubbe.onrender.com/api/${url}`, post, {
         headers: { Authorization: token }
     })
     return res;
 }
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`/api/${url}`, post, {
+    const res = await axios.put(`https://researchhubbe.onrender.com/api/${url}`, post, {
         headers: { Authorization: token }
     })
     return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`/api/${url}`, post, {
+    const res = await axios.patch(`https://researchhubbe.onrender.com/api/${url}`, post, {
         headers: { Authorization: token }
     })
     return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`/api/${url}`, {
+    const res = await axios.delete(`https://researchhubbe.onrender.com/api/${url}`, {
         headers: { Authorization: token }
     })
     return res;
